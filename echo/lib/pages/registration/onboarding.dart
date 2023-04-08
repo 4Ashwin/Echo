0051a57aa16c01c0d23e9b82413874d3622ab889
ASHWIN <75925525+4Ashwin@users.noreply.github.com>
2023-04-08 10:21:30 +0530

    test

---
+++ echo/lib/pages/registration/onboarding.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'dart:ui';
+
+import '../../constants.dart';
+import '../../widgets/login_button.dart';
+
+class onboarding extends StatelessWidget {
+  const onboarding({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Echo'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Center(
+              child: Image(
+                image: AssetImage('images/logo.png'),
+                width: 100,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Echo',
+              style: kHeaderTextStyle,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'voice based email for the blind .',
+              style: kParaTextStyle,
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            LoginButton(
+              image: 'images/google_logo.png',
+              text: 'Sign In with Google',
+              onPressed: () {
+                print('button pressed');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
