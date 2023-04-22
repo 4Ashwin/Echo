2e66dcbd93065091c5a561969721c6c4549f6157
Nooha <noohakottangodan@gmail.com>
2023-04-22 21:53:53 +0530

    compose page stt implemented

--- echo/lib/main.dart
+++ echo/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:echo/pages/registration/onboarding.dart';
+import 'package:echo/pages/compose/compose.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: onboarding(),
+      home: ComposePage(),
     );
   }
 }
