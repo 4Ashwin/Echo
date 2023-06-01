7e64b6eed951370d2d09789712a9a4438230204b
Nooha <noohakottangodan@gmail.com>
2023-06-01 18:54:09 +0530

    onboarding commands

--- echo/lib/pages/registration/onboarding.dart
+++ echo/lib/pages/registration/onboarding.dart
@@ -9,17 +9,72 @@ import '../../Provider/google_sign_in.dart';
 import '../../constants.dart';
 import '../../widgets/buttons/login_button.dart';
 import 'package:flutter_tts/flutter_tts.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
 
 FlutterTts _flutterTts = FlutterTts();
-final TextEditingController _welcomeTextController =
-    TextEditingController(text: "Welcome to the Echo");
-void initState() {
-  // super.initState();
+stt.SpeechToText _speechToText = stt.SpeechToText();
 
-  _flutterTts.speak(_welcomeTextController.text);
+class onboarding extends StatefulWidget {
+  @override
+  _OnboardingState createState() => _OnboardingState();
 }
 
-class onboarding extends StatelessWidget {
+class _OnboardingState extends State<onboarding> {
+  bool _isListening = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _speakWelcomeMessage();
+    _listen();
+  }
+
+  @override
+  void dispose() {
+    _speechToText.stop();
+    super.dispose();
+  }
+
+  void _speakWelcomeMessage() async {
+    await _flutterTts.speak(
+      'Welcome to Echo, your voice-based email assistant. Say "Gmail login" to select a Gmail account. Say "Exit" to exit the app.',
+    );
+  }
+
+  void _listen() async {
+    if (!_isListening) {
+      bool available = await _speechToText.initialize();
+      if (available) {
+        setState(() {
+          _isListening = true;
+        });
+        _speechToText.listen(
+          onResult: (result) {
+            String spokenText = result.recognizedWords.toLowerCase();
+            _processUserInput(spokenText);
+          },
+        );
+      }
+    } else {
+      _speechToText.stop();
+      setState(() {
+        _isListening = false;
+      });
+    }
+  }
+
+  void _processUserInput(String input) {
+    if (input.contains('gmail') || input.contains('login')) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => Profile()),
+      );
+    } else if (input.contains('exit')) {
+      // Close the app
+      Navigator.pop(context);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,23 +92,17 @@ class onboarding extends StatelessWidget {
                 width: 100,
               ),
             ),
-            SizedBox(
-              height: 10,
-            ),
+            SizedBox(height: 10),
             Text(
               'Echo',
               style: kHeaderTextStyle,
             ),
-            SizedBox(
-              height: 10,
-            ),
+            SizedBox(height: 10),
             Text(
-              'voice based email for the blind .',
+              'Voice-based email for the blind.',
               style: kParaTextStyle,
             ),
-            SizedBox(
-              height: 100,
-            ),
+            SizedBox(height: 100),
             LoginButton(
               image: 'images/google_logo.png',
               text: 'Sign In with Google',
@@ -61,6 +110,11 @@ class onboarding extends StatelessWidget {
             ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _listen,
+        child: Icon(_isListening ? Icons.mic : Icons.mic_none),
+        backgroundColor: _isListening ? Colors.red : Colors.blue,
       ),
     );
   }
