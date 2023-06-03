f648d154ab04a901c2a80d6d2d81e19c95840b58
deepak <deepak10pariyapurath@gmail.com>
2023-06-03 23:34:11 +0530

    voice based navigation

--- echo/lib/widgets/buttons/recorderButton.dart
+++ echo/lib/widgets/buttons/recorderButton.dart
@@ -1,3 +1,8 @@
+import 'package:echo/pages/compose/compose.dart';
+import 'package:echo/pages/draft/draft.dart';
+import 'package:echo/pages/home/home.dart';
+import 'package:echo/pages/read/read.dart';
+import 'package:echo/pages/settings/settings.dart';
 import 'package:flutter/material.dart';
 import 'package:speech_to_text/speech_recognition_result.dart';
 import 'package:speech_to_text/speech_to_text.dart';
@@ -58,14 +63,43 @@ class _RecorderButtonState extends State<RecorderButton> {
         _userTextController.text = result.recognizedWords;
       });
       widget.onTextUpdated(result.recognizedWords);
+
       if (result.finalResult) {
+        var input = _userTextController.text.toLowerCase();
         if (_userTextController.text.toLowerCase() == "quit") {
           _stopListening();
         } else {
-          _flutterTts.speak("User input received: ${_userTextController.text}");
+          
+          _flutterTts.speak("User input received yes: ${_userTextController.text}");
           _addMessage(_userTextController.text, true);
           _addMessage("User input received: ${_userTextController.text}",
               false); // new line
+          if(input=="echo combos"||input=="echo combo"||input=="eco combo"||input=="echo compose"||input=="compose"||input=="combos"||input=="compose email" ||input=="email compose")
+          {
+            Navigator.push(context,MaterialPageRoute(builder: (context) =>ComposePage()));
+          }
+          else if(input=="draft"||input=="echo draft"||input=="eco draft" ||input=="draft email")
+          {
+            Navigator.push(context,MaterialPageRoute(builder: (context) =>DraftPage()));
+          }
+          else if(input=="inbox"||input=="echo inbox"||input=="eco inbox" ||input=="read email")
+          {
+            Navigator.push(context,MaterialPageRoute(builder: (context) =>ReadPage()));
+          }
+          else if(input=="settings"||input=="setting"||input=="eco settings" ||input=="echo setting"||input=="echo settings")
+          {
+            Navigator.push(context,MaterialPageRoute(builder: (context) =>Settings()));
+          }
+          else if(input=="echo home"||input=="home"||input=="eco home" ||input=="home page")
+          {
+            Navigator.push(context,MaterialPageRoute(builder: (context) =>Home()));
+          }
+          else if(input=="echo back" ||input=="eco back"||input=="eco go back"||input=="ecoback")
+          {
+            Navigator.push(context,MaterialPageRoute(builder: (context) =>Home()));
+            
+            // Navigator.pop(context);
+          }
           _userTextController.clear();
         }
       } // Call the callback function
