e75f5c4da58994233f58a134d36443eccea1ce7d
ASHWIN <75925525+4Ashwin@users.noreply.github.com>
2023-06-17 19:44:35 +0530

    test ash

--- echo/lib/Provider/google_sign_in.dart
+++ echo/lib/Provider/google_sign_in.dart
@@ -11,41 +11,43 @@ class GoogleSignInProvider extends ChangeNotifier {
 
   Future googleLogin(BuildContext context, Widget widget) async {
     try {
-      
       final googleUser = await googleSignIn.signIn();
       if (googleUser == null) return;
       _user = googleUser;
       final googleAuth = await googleUser.authentication;
+      print(googleAuth);
+      print('haha');
       final credential = GoogleAuthProvider.credential(
         accessToken: googleAuth.accessToken,
         idToken: googleAuth.idToken,
       );
       await FirebaseAuth.instance.signInWithCredential(credential);
+
       Navigator.push(
-            context,
-            PageRouteBuilder(
-                pageBuilder: (BuildContext context, Animation animation,
-                    Animation secondaryAnimation) {
-                  return widget;
-                },
-                opaque: true,
-                barrierColor: Colors.grey,
-                transitionDuration: Duration(milliseconds: 800),
-                transitionsBuilder: (BuildContext context,
-                    Animation<double> animation,
-                    Animation<double> secondaryAnimation,
-                    Widget child) {
-                  return FadeTransition(
-                    opacity: animation,
-                    child: SlideTransition(
-                      position: new Tween<Offset>(
-                        begin: const Offset(0.0, 1.0),
-                        end: Offset.zero,
-                      ).animate(animation),
-                      child: child,
-                    ),
-                  );
-                }));
+          context,
+          PageRouteBuilder(
+              pageBuilder: (BuildContext context, Animation animation,
+                  Animation secondaryAnimation) {
+                return widget;
+              },
+              opaque: true,
+              barrierColor: Colors.grey,
+              transitionDuration: Duration(milliseconds: 800),
+              transitionsBuilder: (BuildContext context,
+                  Animation<double> animation,
+                  Animation<double> secondaryAnimation,
+                  Widget child) {
+                return FadeTransition(
+                  opacity: animation,
+                  child: SlideTransition(
+                    position: new Tween<Offset>(
+                      begin: const Offset(0.0, 1.0),
+                      end: Offset.zero,
+                    ).animate(animation),
+                    child: child,
+                  ),
+                );
+              }));
     } catch (e) {
       print(e.toString());
     }
