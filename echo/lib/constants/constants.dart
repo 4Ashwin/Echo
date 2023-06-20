491aa5f7c3059b9d174dc70a0a61b73e5859736d
Nooha <noohakottangodan@gmail.com>
2023-06-21 01:04:42 +0530

    .

--- echo/lib/constants/constants.dart
+++ echo/lib/constants/constants.dart
@@ -1,7 +1,6 @@
-class Constants{
+class Constants {
   static String accesstoken = '';
   static String theuser = ''; //email id
   static String nameuser = ''; //username
-static List<String> Data_to_send = [];
-
+  static List<String> Data_to_send = [];
 }
